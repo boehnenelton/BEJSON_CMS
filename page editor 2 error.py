5ff3ee87e146b1dae6ec73da8a0b029f8027410e
Elton Boehnen <boehnenelton2024@gmail.com>
2026-06-15 00:50:47 -0700

    Auto-commit: System-wide synchronization - 2026-06-15

---
+++ page editor 2 error.py
@@ -0,0 +1,14 @@
+PAGE EDITOR V2 ERROR.
+
+
+Traceback (most recent call last):
+  File "/data/user/0/ru.iiec.pydroid3/files/accomp_files/iiec_run/iiec_run.py", line 31, in <module>
+    start(fakepyfile,mainpyfile)
+    ~~~~~^^^^^^^^^^^^^^^^^^^^^^^
+  File "/data/user/0/ru.iiec.pydroid3/files/accomp_files/iiec_run/iiec_run.py", line 30, in start
+    exec(open(mainpyfile).read(),  __main__.__dict__)
+    ~~~~^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "<string>", line 42, in <module>
+NameError: name 'app' is not defined
+
+[Program finished]
